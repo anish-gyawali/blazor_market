c4bd1886e4dc9680b3dfb5e2db2c107bf8378f7d
Anish Gyawali <anish.gyawali7@gmail.com>
2023-09-07 16:51:48 -0500

    Login and registration form created and updated route

--- Blazor_Market/Models/User.cs
+++ Blazor_Market/Models/User.cs
@@ -5,7 +5,7 @@
         public string? Id { get; set; }  
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? Username { get; set; }
+        public string? Email { get; set; }
         public string? Password { get; set; }
         public string? Token { get; set; }
     }
